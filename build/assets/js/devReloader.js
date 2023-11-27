affc37c996e6641fdb43f21c6465aca4fa7dd4c2
Tim Spaulding <tspauld98@gmail.com>
2023-11-26 21:42:28 -0800

    Added build dir to gitignore

--- build/assets/js/devReloader.js
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
- * Copyright (c) 2023 Battle Road Consulting. All rights reserved.
- *******************************************************************************/
-
-if (window.location.hostname === "localhost") {
-    var devReloaderScript = document.createElement("script");
-    devReloaderScript.src = "//localhost:35729/livereload.js";
-    document.body.appendChild(devReloaderScript);
-}
